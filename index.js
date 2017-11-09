5490a24a300dc049adef8279170a781dc2c87a67
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-09 23:44:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  switch(string) {
-    case string.toLowerCase() === string:
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
-    case string.toUpperCase() === string:
+  }
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-    case string === "I love you, Grandma.":
+  }
+  else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
