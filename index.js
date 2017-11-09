aa0c7201bf50de2fac4e929bd8d9ea3783146d18
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-09 23:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  switch(string) {
+    case string.toLowerCase === lowercase:
+    return "I can't hear you!";
+    case string.toUpperCase === uppercase:
+    return "YES INDEED!";
+    case string === "I love you, Grandma.":
+    return "I love you, too.";
+  }
+}
